59a604f8105d178bebeb70df0515645023dde91b
Nicolás Rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2023-10-14 17:19:01 -0300

    Add getMovies method to Movies Service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from 'src/app/services/movies.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  movies: any = [];
 
+  constructor(private _moviesService: MoviesService) {}
+
+  ngOnInit(): void {
+    this._moviesService.getMovies().subscribe((movies: any) => {
+      this.movies = movies.results;
+      console.log({ movies });
+    });
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MoviesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getMovies() {
+    return this.httpClient.get(
+      'https://api.themoviedb.org/3/movie/upcoming?api_key=08c319c267bed792d6bcc69617edca9f'
+    );
+  }
 }
