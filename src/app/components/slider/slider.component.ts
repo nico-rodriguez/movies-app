c97f20616d09cf9e6f523ef2e8e8c9902935c152
Nicolás Rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2023-10-14 23:03:18 -0300

    Use component data in Slider component

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Movie } from 'src/app/modesl/movie.model';
 
 @Component({
   selector: 'slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss'],
 })
-export class SliderComponent {}
+export class SliderComponent {
+  @Input() items: Movie[] = [];
+}
