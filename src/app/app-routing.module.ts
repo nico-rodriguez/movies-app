b7e191f868a09fd336c2cc22abef2f3a4ad12aa9
Nicolás Rodriguez <18080513+nico-rodriguez@users.noreply.github.com>
2023-12-03 20:35:01 -0300

    Get Movies by Genre

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     component: MoviesComponent,
   },
   {
+    path: 'movies/genres/:genreId',
+    component: MoviesComponent,
+  },
+  {
     path: 'movie/:id',
     component: MovieComponent,
   },
--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,7 +1,9 @@
+import { take } from 'rxjs';
 import { Movie } from 'src/app/models/movie.model';
 import { MoviesService } from 'src/app/services/movies.service';
 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movies',
@@ -11,11 +13,23 @@ import { Component, OnInit } from '@angular/core';
 export class MoviesComponent implements OnInit {
   public movies: Movie[] = [];
 
-  constructor(private _moviesService: MoviesService) {}
+  constructor(
+    private _moviesService: MoviesService,
+    private _route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    return;
-    this.getMoviesPage(0);
+    this._route.params.pipe(take(1)).subscribe(({ genreId }) => {
+      if (genreId) {
+        this.getMoviesByGenre(genreId);
+      } else {
+        this.getMoviesPage(0);
+      }
+    });
+  }
+
+  getMoviesByGenre(genreId: any) {
+    throw new Error('Method not implemented.');
   }
 
   getMoviesPage(page: number) {
